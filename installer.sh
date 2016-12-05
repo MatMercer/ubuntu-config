d781f0600e633bc561a25537b040ef6eb62b19be
MrAnyone <m3rc3r99@gmail.com>
2016-12-04 22:01:11 -0200

    Initial commit

---
+++ installer.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Constants
+WARN='\033[1;33m' # Yellow
+NC='\033[0m' # No Color
+
+# vim installer
+function install_vim () {
+    sudo apt-get install vim-gnome
+    wget -O - http://j.mp/spf13-vim3 | sh
+}
+
+# zsh installer
+function install_zsh () {
+    printf "${WARN}Going to install zsh and oh-my-zsh, please logout after fully installed to use it.\n${NC}"
+    printf "${WARN}Installing zsh.\n${NC}"
+    sudo apt-get install zsh
+    printf "${WARN}Installing oh-my-zsh.\n${NC}"
+    sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+}
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -v | --vim)             shift
+                                install_vim
+                                ;;
+        -z | --zsh)             install_zsh
+                                ;;
+        -h | --help )           usage
+                                exit
+                                ;;
+        * )                     usage
+                                exit 1
+    esac
+    shift
+done
