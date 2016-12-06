116b51e07a5278533e9e70c51c80fcbe1c9ccd61
MrAnyone <m3rc3r99@gmail.com>
2016-12-06 10:57:22 -0200

    Add help command

--- installer.sh
+++ installer.sh
@@ -4,6 +4,14 @@
 WARN='\033[1;33m' # Yellow
 NC='\033[0m' # No Color
 
+# usage description
+function usage () {
+    printf "This file installs different utilities in your system.\nThe usage is as follow:\n"
+    printf "\t-v --vim\tinstall spf13 vim\n"
+    printf "\t-z --zsh\tinstall zsh\n"
+    printf "\t-h --help\tdisplay usage\n\0"
+}
+
 # vim installer
 function install_vim () {
     sudo apt-get install vim-gnome
@@ -18,6 +26,11 @@ function install_zsh () {
     printf "${WARN}Installing oh-my-zsh.\n${NC}"
     sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 }
+
+if [ $# -eq 0 ]
+then
+    usage
+fi
 
 while [ "$1" != "" ]; do
     case $1 in
