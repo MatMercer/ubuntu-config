e45522afa659e39bc0fbb7c4a4d0c5644c7c0bd5
MrAnyone <m3rc3r99@gmail.com>
2016-12-08 16:32:59 -0200

    Add nodejs installer

--- .zshrc
+++ .zshrc
@@ -94,3 +94,13 @@ function zle-line-init zle-keymap-select {
 }
 zle -N zle-line-init
 zle -N zle-keymap-select
+export PATH=~/.npm-global/bin:$PATH
+
+# NPM configuration
+export NPM_PACKAGES="/home/mranyone/.npm-packages"
+export NODE_PATH="$NPM_PACKAGES/lib/node_modules${NODE_PATH:+:$NODE_PATH}"
+export PATH="$NPM_PACKAGES/bin:$PATH"
+# Unset manpath so we can inherit from /etc/manpath via the `manpath`
+# command
+unset MANPATH  # delete if you already modified MANPATH elsewhere in your config
+export MANPATH="$NPM_PACKAGES/share/man:$(manpath)"
--- installer.sh
+++ installer.sh
@@ -4,6 +4,9 @@
 WARN='\033[1;33m' # Yellow
 NC='\033[0m' # No Color
 
+# Global variables
+BASEDIR='.'
+
 # usage description
 function usage () {
     printf "This file installs different utilities in your system.\nThe usage is as follow:\n"
@@ -25,7 +28,45 @@ function install_zsh () {
     sudo apt-get install zsh
     printf "${WARN}Installing oh-my-zsh.\n${NC}"
     sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-    cat ./.zshrc > ~/.zshrc
+    update_zshrc
+    fix_zsh_secure_folder
+}
+
+# npm and nodejs installer
+function install_nodejs () {
+    printf "${WARN}Going to install node js and npm.\n${NC}"
+
+    printf "${WARN}Installing nodejs.\n${NC}"
+    sudo apt-get install nodejs -y
+    printf "${WARN}Creating node sym link...\n${NC}"
+    sudo ln -s /usr/bin/nodejs /usr/bin/node
+    update_zshrc
+
+    printf "${WARN}Installing npm.\n${NC}"
+    sudo apt-get install npm -y
+    printf "${WARN}Going to fix npm perms...\n${NC}"
+    wget -O- https://raw.githubusercontent.com/glenpike/npm-g_nosudo/master/npm-g-nosudo.sh | sh
+    fix_zsh_secure_folder
+}
+
+function update_basedir () {
+    BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+}
+
+update_basedir
+
+# update home zshrc
+function update_zshrc () {
+    printf "${WARN}Updating zshrc...\n${NC}";
+    cat $BASEDIR/.zshrc > ~/.zshrc
+}
+
+# Fix zsh prompting insecure folders
+function fix_zsh_secure_folder () {
+    printf "${WARN}Changing zsh site-functions perms...\n${NC}";
+    cd /usr/local/share/zsh
+    sudo chmod -R 755 ./site-functions
+    cd $BASEDIR
 }
 
 if [ $# -eq 0 ]
@@ -39,6 +80,10 @@ while [ "$1" != "" ]; do
                                 install_vim
                                 ;;
         -z | --zsh)             install_zsh
+                                ;;
+        -n | --node)            install_nodejs
+                                ;;
+        -u | --updatezshrc)     update_zshrc
                                 ;;
         -h | --help )           usage
                                 exit
