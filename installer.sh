204035e8aaec50a826d622dd693b6c4bb0670df2
MtMercer <mateusmercer@gmail.com>
2017-01-20 16:01:41 -0200

    Implement warn function

--- installer.sh
+++ installer.sh
@@ -14,15 +14,20 @@ function update_basedir () {
 
 update_basedir
 
+# Warn function
+function warn () {
+    printf "${WARN}$1\n${NC}";
+}
+
 # update home zshrc
 function update_zshrc () {
-    printf "${WARN}Updating zshrc...\n${NC}";
+    warn "Updating zshrc...";
     cat $BASEDIR/.zshrc > ~/.zshrc
 }
 
 # Fix zsh prompting insecure folders
 function fix_zsh_secure_folder () {
-    printf "${WARN}Changing zsh site-functions perms...\n${NC}";
+    warn "Changing zsh site-functions perms...";
     cd /usr/local/share/zsh
     sudo chmod -R 755 ./site-functions
     cd $BASEDIR
@@ -30,7 +35,7 @@ function fix_zsh_secure_folder () {
 
 # usage description
 function usage () {
-    printf "This file installs different utilities in your system.\nThe usage is as follow:\n"
+    warn "This file installs different utilities in your system.\nThe usage is as follow:"
     printf "\t-j8 --java-8\tinstall Java 8\n"
     printf "\t-m --mysql_5_6\tinstall mySQL 5.6\n"
     printf "\t-n --node\tinstall nodejs\n"
@@ -42,16 +47,17 @@ function usage () {
 
 # vim installer
 function install_vim () {
+    warn "Going to install vim..."
     sudo apt-get install vim-gnome
     wget -O - http://j.mp/spf13-vim3 | sh
 }
 
 # zsh installer
 function install_zsh () {
-    printf "${WARN}Going to install zsh and oh-my-zsh, please logout after fully installed to use it.\n${NC}"
-    printf "${WARN}Installing zsh.\n${NC}"
+    warn "Going to install zsh and oh-my-zsh, please logout after fully installed to use it."
+    warn "Installing zsh."
     sudo apt-get install zsh
-    printf "${WARN}Installing oh-my-zsh.\n${NC}"
+    warn "Installing oh-my-zsh."
     sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
     update_zshrc
     fix_zsh_secure_folder
@@ -59,8 +65,8 @@ function install_zsh () {
 
 # sublime text installer
 function install_sublime () {
-    printf "${WARN}Going to install sublime text 3.\n${NC}"
-    printf "${WARN}Installing sublime text 3.\n${NC}"
+    warn "Going to install sublime text 3."
+    warn "Installing sublime text 3."
     sudo add-apt-repository ppa:webupd8team/sublime-text-3
     sudo apt-get update
     sudo apt-get install sublime-text
@@ -68,26 +74,26 @@ function install_sublime () {
 
 # npm and nodejs installer
 function install_nodejs () {
-    printf "${WARN}Going to install node js and npm.\n${NC}"
+    warn "Going to install node js and npm."
 
-    printf "${WARN}Installing nodejs.\n${NC}"
+    warn "Installing nodejs."
     sudo apt-get install nodejs -y
-    printf "${WARN}Creating node sym link...\n${NC}"
+    warn "Creating node sym link..."
     sudo ln -s /usr/bin/nodejs /usr/bin/node
     update_zshrc
 
-    printf "${WARN}Installing npm.\n${NC}"
+    warn "Installing npm."
     sudo apt-get install npm -y
-    printf "${WARN}Going to fix npm perms...\n${NC}"
+    warn "Going to fix npm perms..."
     wget -O- https://raw.githubusercontent.com/glenpike/npm-g_nosudo/master/npm-g-nosudo.sh | sh
     fix_zsh_secure_folder
 }
 
 # java 8 installer
 function install_java_8 () {
-    printf "${WARN}Going to install Java 8.\n${NC}"
+    warn "Going to install Java 8."
 
-    printf "${WARN}Installing Java 8.\n${NC}"
+    warn "Installing Java 8."
     sudo add-apt-repository ppa:webupd8team/java
     sudo apt-get update
     sudo apt-get install oracle-java8-installer
@@ -96,9 +102,9 @@ function install_java_8 () {
 
 # mysql 5.6 installer
 function install_mysql_5_6 () {
-    printf "${WARN}Going to MySQL 5.6.\n${NC}"
+    warn "Going to MySQL 5.6."
 
-    printf "${WARN}Installing MySQL 5.6.\n${NC}"
+    warn "Installing MySQL 5.6."
     sudo apt-get update
     sudo apt-get install mysql-server
     sudo mysql_secure_installation
